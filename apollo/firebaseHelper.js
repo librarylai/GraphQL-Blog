e2473da13b8c7fbf6a2d1b4daff78349ef0ebf74
librarylai <library0515@gmail.com>
2022-02-19 19:18:43 +0800

    feat: 增加套件

--- apollo/firebaseHelper.js
+++ apollo/firebaseHelper.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getDatabase, ref, child, get } from 'firebase/database'
+import { getDatabase, ref, child, get, set } from 'firebase/database'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig)
 const database = ref(getDatabase(firebase))
-export { database, child, get, firebase }
+// export  { database, child, get, set }
+export default database
